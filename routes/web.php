ba047a9961ccfd6f8745a5940a85dc879eb50c5b
Paulo Vargatt <pvargatt@gmail.com>
2018-06-01 18:16:09 -0300

    post mutator img

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,8 @@
 <?php
 
 
-Route::get('/', function () {
-    return view('blog.index');
-});
+Route::get('/', 'BlogController@index')->name('index');
+
 
 Route::get('/blog/show', function () {
     return view('blog.show');
