e5c233e6933cea91a21056054bfb1f6203534cdd
Paulo Vargatt <pvargatt@gmail.com>
2018-06-02 18:06:45 -0300

    correções e footer

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.main')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 text-center text-capitalize">
+            <h1>Página não encontrada</h1>
+            <br>
+        </div>
+    </div>
+</div>
+@endsection
